ba38421ada8f7ce066b8b87a3c6e7152e49f7a9f
esscott1 <ericscott411@gmail.com>
2019-10-23 08:15:13 -0600

    Add project files.

---
+++ gcp_web_core_test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gcp_web_core_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
