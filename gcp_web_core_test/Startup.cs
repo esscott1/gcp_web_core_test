6263468ed3e758fb1e74992ee9658457a9b882b1
esscott1 <ericscott411@gmail.com>
2019-10-23 08:38:45 -0600

    update to framework 2.1

--- gcp_web_core_test/Startup.cs
+++ gcp_web_core_test/Startup.cs
@@ -31,7 +31,7 @@ namespace gcp_web_core_test
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
